3083be783815df5db663cd7dc4db224ade432287
Gavin.Wakeley <gavin.wakeley@bjss.com>
2021-02-03 20:24:42 +0000

    Add appgw module

---
+++ bc_app_gateway_ingress/output.tf
@@ -0,0 +1,10 @@
+output "appgw_name" {
+    description = "The Application Gateway Name"
+    value = azurerm_application_gateway.AppGw.name
+    sensitive   = false
+}
+
+output "appgw_pip_fqdn" {
+    description = "The Application Gateway Public Name"
+    value = azurerm_public_ip.PipAppGw.fqdn
+}
